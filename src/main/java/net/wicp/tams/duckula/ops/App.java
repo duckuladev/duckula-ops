b23edf7c74db773e81440e9adcf8377e0c18cf0d
andy <rjzjh@163.com>
2020-12-19 20:12:39 +0800

    k8s支持

--- src/main/java/net/wicp/tams/duckula/ops/App.java
+++ src/main/java/net/wicp/tams/duckula/ops/App.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 import net.wicp.tams.app.duckula.controller.BusiTools;
+import net.wicp.tams.common.Conf;
 import net.wicp.tams.common.spring.autoconfig.annotation.EnableTams;
 
 @SpringBootApplication
@@ -24,6 +25,8 @@ public class App extends SpringBootServletInitializer {
 	}
 
 	public static void main(String[] args) throws Exception {
+		// 在k8s里使用此目录，所以duckula支持2种配置，在bootstrap.yml文件里配置的，和下面这种方式设置的文件都支持。且下面这个配置文件优先级更高
+		Conf.overProp("common.spring.autoconfig.property.path", "abs:/data/duckula-data/conf/duckula-ops.properties");
 		SpringApplication application = new SpringApplication(App.class);
 		application.setApplicationContextClass(AnnotationConfigWebApplicationContext.class);
 		BusiTools.printAscill();
