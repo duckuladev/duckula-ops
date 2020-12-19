c4a89f87819cfd3ebb51fe33bb12dbf041ba8779
andy <rjzjh@163.com>
2020-12-19 16:11:41 +0800

    切换配置文件

--- src/main/java/net/wicp/tams/duckula/ops/App.java
+++ src/main/java/net/wicp/tams/duckula/ops/App.java
@@ -10,23 +10,20 @@ import org.springframework.context.annotation.ImportResource;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 import net.wicp.tams.app.duckula.controller.BusiTools;
-import net.wicp.tams.common.Conf;
 import net.wicp.tams.common.spring.autoconfig.annotation.EnableTams;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "net.wicp.tams.app.duckula.controller", "net.wicp.tams.duckula.ops" })
 @ImportResource("classpath:beanRefContext.xml")
 @MapperScan("net.wicp.tams.app.duckula.controller.dao")
-@EnableTams
+@EnableTams(configpath = "home:/duckula-ops.properties")
 public class App extends SpringBootServletInitializer {
-
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(AppConfiguration.class);
 	}
 
 	public static void main(String[] args) throws Exception {
-		Conf.overProp("common.spring.autoconfig.property.path", "abs:/data/duckula-data/conf/application.properties");
 		SpringApplication application = new SpringApplication(App.class);
 		application.setApplicationContextClass(AnnotationConfigWebApplicationContext.class);
 		BusiTools.printAscill();
